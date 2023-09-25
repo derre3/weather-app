dc39783f86aa80b4338d1c1f6ad9d4d133624631
andre <123985988+derre3@users.noreply.github.com>
2023-09-25 11:22:06 -0300

    fill info-grid layout with fetched data

--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,9 @@
-import { fetchData } from './index';
+import { fetchData } from './weather';
 
 async function searchLocation() {
   const data = await fetchData(searchInput.value);
   updateInfo(data);
-  console.log(data);
+  // console.log(data);
   searchInput.value = '';
 }
 
@@ -11,18 +11,31 @@ function updateInfo(data) {
   let useUnit;
   if (unit === 0) useUnit = data.metric;
   else useUnit = data.imperial;
+  const date = new Date(data.localTime);
+
   infoLocation[0].textContent = `${data.name}, ${data.country}`;
-  infoLocation[1].textContent = `${data.localTime}`;
+  infoLocation[1].textContent = `${date}`;
 
   infoTemp[0].src = `${data.condition.icon}`;
   infoTemp[1].textContent = `${useUnit.temp}`;
   infoTemp[2].textContent = `${data.condition.text}`;
+
+  infoGrid[0].textContent = `Feels Like: ${useUnit.feelsLike}`;
+  infoGrid[1].textContent = `Humidity: ${data.humidity}`;
+  infoGrid[2].textContent = `Precipitation: ${useUnit.precip}`;
+  infoGrid[3].textContent = `Wind Direction: ${data.wind_dir}`;
+  infoGrid[4].textContent = `Wind Speed: ${useUnit.wind}`;
+  infoGrid[5].textContent = `Wind Gust: ${useUnit.gust}`;
+  infoGrid[6].textContent = `Visibility: ${useUnit.vis}`;
+  infoGrid[7].textContent = `Atmospheric Pressure: ${useUnit.pressure}`;
+  infoGrid[8].textContent = `UV index: ${data.uv}`;
 }
 
 const searchInput = document.querySelector('#search-input');
 const searchButton = document.querySelector('#search-button');
 const infoLocation = document.querySelectorAll('#info-location *');
 const infoTemp = document.querySelectorAll('#info-temp *');
+const infoGrid = document.querySelectorAll('#info-grid *');
 const metricButton = document.querySelector('#metric');
 const imperialButton = document.querySelector('#imperial');
 
