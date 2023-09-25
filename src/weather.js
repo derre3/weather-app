3db81b76c73239a8bd15d8a6ca7e1bcde32c7754
andre <123985988+derre3@users.noreply.github.com>
2023-09-25 13:01:36 -0300

    add forecast fetch

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,30 @@
 /* eslint-disable import/prefer-default-export */
 
 function processData(data) {
-  const object = {
+  const forecastDays = data.forecast.forecastday;
+  const forecastProcessed = [];
+  forecastDays.forEach((forecast) => {
+    const forecastObj = {
+      date: forecast.date,
+      condition: {
+        text: forecast.day.condition.text,
+        icon: forecast.day.condition.icon,
+      },
+
+      metric: {
+        maxtemp: `${forecast.day.maxtemp_c} ºC`,
+        mintemp: `${forecast.day.mintemp_c} ºC`,
+      },
+
+      imperial: {
+        maxtemp: `${forecast.day.maxtemp_f} ºF`,
+        mintemp: `${forecast.day.mintemp_f} ºF`,
+      },
+    };
+    forecastProcessed.push(forecastObj);
+  });
+
+  const weatherObj = {
     name: data.location.name,
     region: data.location.region,
     country: data.location.country,
@@ -34,14 +57,16 @@ function processData(data) {
       vis: `${data.current.vis_miles} miles`,
       wind: `${data.current.wind_mph} mph`,
     },
+
+    forecast: forecastProcessed,
   };
 
-  return object;
+  return weatherObj;
 }
 
-async function fetchData(location) {
+async function fetchWeather(location) {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=c36ab4bfb1c341c285f151048231909&q=${location}`,
+    `https://api.weatherapi.com/v1/forecast.json?key=c36ab4bfb1c341c285f151048231909&q=${location}&days=3`,
     { mode: 'cors' }
   );
 
@@ -49,7 +74,8 @@ async function fetchData(location) {
   const data = processData(rawData);
 
   //   console.log(rawData);
+  //   console.log(data);
   return data;
 }
 
-export { fetchData };
+export { fetchWeather };
