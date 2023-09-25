b726a5370c168c2b5346f7094d6bca6bf6e71ed5
andre <123985988+derre3@users.noreply.github.com>
2023-09-25 14:15:06 -0300

    add forecast to UI

--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,15 @@
-import { fetchData } from './weather';
+import { fetchWeather } from './weather';
 
 async function searchLocation() {
-  const data = await fetchData(searchInput.value);
+  const data = await fetchWeather(searchInput.value);
+  if (infoForecast.children) {
+    // eslint-disable-next-line no-plusplus
+    const childrenLength = infoForecast.children.length;
+    for (let i = 0; i < childrenLength; i += 1) {
+      infoForecast.lastChild.remove();
+    }
+  }
   updateInfo(data);
-  // console.log(data);
   searchInput.value = '';
 }
 
@@ -11,10 +17,9 @@ function updateInfo(data) {
   let useUnit;
   if (unit === 0) useUnit = data.metric;
   else useUnit = data.imperial;
-  const date = new Date(data.localTime);
 
   infoLocation[0].textContent = `${data.name}, ${data.country}`;
-  infoLocation[1].textContent = `${date}`;
+  infoLocation[1].textContent = `${data.localTime}`;
 
   infoTemp[0].src = `${data.condition.icon}`;
   infoTemp[1].textContent = `${useUnit.temp}`;
@@ -29,6 +34,34 @@ function updateInfo(data) {
   infoGrid[6].textContent = `Visibility: ${useUnit.vis}`;
   infoGrid[7].textContent = `Atmospheric Pressure: ${useUnit.pressure}`;
   infoGrid[8].textContent = `UV index: ${data.uv}`;
+
+  data.forecast.forEach((day) => {
+    if (unit === 0) useUnit = day.metric;
+    else useUnit = day.imperial;
+
+    const card = document.createElement('div');
+    card.classList.add('forecast-card');
+    const dateElement = document.createElement('p');
+    const imgElement = document.createElement('img');
+    const minTempElement = document.createElement('p');
+    const maxTempElement = document.createElement('p');
+    const conditionElement = document.createElement('p');
+
+    dateElement.textContent = day.date;
+    imgElement.src = day.condition.icon;
+    minTempElement.textContent = `Min: ${useUnit.mintemp}`;
+    maxTempElement.textContent = `Max: ${useUnit.maxtemp}`;
+    conditionElement.textContent = day.condition.text;
+
+    card.append(
+      dateElement,
+      imgElement,
+      minTempElement,
+      maxTempElement,
+      conditionElement
+    );
+    infoForecast.append(card);
+  });
 }
 
 const searchInput = document.querySelector('#search-input');
@@ -36,6 +69,7 @@ const searchButton = document.querySelector('#search-button');
 const infoLocation = document.querySelectorAll('#info-location *');
 const infoTemp = document.querySelectorAll('#info-temp *');
 const infoGrid = document.querySelectorAll('#info-grid *');
+const infoForecast = document.querySelector('#info-forecast');
 const metricButton = document.querySelector('#metric');
 const imperialButton = document.querySelector('#imperial');
 
