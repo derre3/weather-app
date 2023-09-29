f668629d487ab3f072e920d7b75a01c31862a65f
andre <123985988+derre3@users.noreply.github.com>
2023-09-29 18:49:44 -0300

    change forecast to 5 days

--- src/weather.js
+++ src/weather.js
@@ -62,7 +62,7 @@ function processData(data) {
 
 async function fetchWeather(location) {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=c36ab4bfb1c341c285f151048231909&q=${location}&days=3`,
+    `https://api.weatherapi.com/v1/forecast.json?key=c36ab4bfb1c341c285f151048231909&q=${location}&days=5`,
     { mode: 'cors' }
   );
 
